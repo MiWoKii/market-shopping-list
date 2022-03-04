3ca7c19902a41b83c4d0d801654eeeeef1ca85d5
MiWoKii <prates.jeferson.jg@gmail.com>
2022-03-04 20:56:43 -0300

    style(types): create types using jsdocs

---
+++ src/components/typedefs.js
@@ -0,0 +1,23 @@
+/**
+ * @typedef {{name: string, value: number, unit: ('unidade'|'kg'), quantity: number}} ShopItem
+ */
+
+/**
+ * @callback FormSetterCallback
+ * @param {ShopItem} prevForm previous value of form
+ */
+
+/**
+ * @callback FormSetter
+ * @param {ShopItem|FormSetterCallback} form new object to save on form
+ */
+
+/**@typedef {('error'|'warning'|'info'|'success')} SnackbarSeverities */
+
+/**@typedef {{show: boolean, message: string, severity: SnackbarSeverities}} SnackBar */
+
+/**
+ * @callback SnackBarSetter
+ * @param {Partial<SnackBar>} snack
+ * @returns {void}
+ */
