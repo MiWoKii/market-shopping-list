d73c535c568c251d34df36da916284e3856db61b
MiWoKii <prates.jeferson.jg@gmail.com>
2022-03-04 21:36:20 -0300

    feat(pages): adds new page to create items

---
+++ src/pages/create-item/index.js
@@ -0,0 +1,10 @@
+import FormItem from "../../components/form-item";
+import Page from "../../components/page";
+
+export default function CreateItem() {
+  return (
+    <Page>
+      <FormItem />
+    </Page>
+  );
+}
